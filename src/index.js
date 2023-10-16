55784c21742d22715aa92709850fe06e91890e07
ferdente <64047548+ferdente@users.noreply.github.com>
2023-10-16 15:06:33 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,42 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+        
+    let arr = []
+
+        for (let i = 0; i < expr.length; i + 10) {
+            arr.push(expr.substring(i, i+10))
+            i = i + 10
+        }
+
+    let arr2 = [];
+
+        arr.forEach((e)=> {
+            let litera = "";
+            for(let i = 0; i < e.length; i++) {
+                if (e[i] == '1'){
+                litera += litera = e[i+1] === '0' ? '.' : "-";
+                i++
+                }
+            }
+            arr2.push(litera)
+        })
+
+    let arr3 = [];
+
+        arr2.forEach((e)=> {
+            if (e == ''){
+                arr3.push(' ')
+            }else{
+        arr3.push(getValue(e, MORSE_TABLE))
+        }
+        })
+
+    function getValue(key, obj) {
+    return obj[key];
+    }
+
+    return arr3.join('')
 }
 
 module.exports = {
